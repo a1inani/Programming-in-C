f03619f21a5dd87f2658f77916ea969611128df6
Alinani Simukanga <alinani10@gmail.com>
2020-05-12 20:07:14 +0200

    Add exercise4.c

---
+++ Chapter_3/exercise4.c
@@ -0,0 +1,13 @@
+/*
+  Write a program that subtracts the value 15 from 87 and displays the result
+  together with an appropriate message, at the terminal
+*/
+#include <stdio.h>
+
+int main (void)
+{
+  int difference = 87 - 15;
+  printf("The difference between 87 and 15 is %i\n", difference);
+
+  return 0;
+}
