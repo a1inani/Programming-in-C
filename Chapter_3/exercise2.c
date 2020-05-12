cb669509def994c2a442a394cf521f0dc0302ad4
Alinani Simukanga <alinani10@gmail.com>
2020-05-12 20:04:08 +0200

    Add exercise2.c

---
+++ Chapter_3/exercise2.c
@@ -0,0 +1,18 @@
+/*
+  Write a program that prints the following text at the terminal:
+    - In C, lowercase letters are significant.
+    - main is where program execution begins.
+    - Opening and closing braces enclose program statements in a routine.
+    - All program statements must be terminated by a semicolon.
+*/
+#include <stdio.h>
+
+int main (void)
+{
+  printf ("In C, lowercase letters are significant.\n");
+  printf ("main is where program execution begins.\n");
+  printf ("Opening and closing braces enclose program statements in a routine.\n");
+  printf ("All program statements must be terminated by a semicolon.\n");
+
+  return 0;
+}
